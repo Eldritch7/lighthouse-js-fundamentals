111ad5356785babe4ba211970a493d0a8eccfcfc
Sara <parakeet_22@yahoo.com>
2022-01-10 18:45:13 -0500

    commentted original code, added console.log and return versions of the code

--- hello.js
+++ hello.js
@@ -1,5 +1,21 @@
-const sayHello  = function (name) {
+/*const sayHello  = function (name) {
   console.log("Hello, "+name);
 }
 
-sayHello("Sara");
+sayHello("Sara");*/
+
+
+//console.log function outupts the result
+const sayHelloToConsole = function(name){
+  console.log("Hello, " +name);
+}
+sayHelloToConsole('John');
+
+
+//return function returns value to variable, no output unless you
+cosole.log it later
+const returnSayHello = function(name){
+  return "hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
